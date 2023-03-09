356ba4c31d96c962d5eaf3d90e8dec822892f028
Byeong Rok <rokany@cau.ac.kr>
2023-03-09 20:50:26 +0900

    r

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,23 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; // router setting
+import Detail from "./routes/Detail"; // router setting
+import Home from "./routes/Home"; // router setting
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/hello">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
